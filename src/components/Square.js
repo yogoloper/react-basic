b080bab93566daa9a7294509f2537418eeb62952
yogoloper <jguddnjs@gmail.com>
2024-02-14 16:18:00 +0900

    board 컴포넌트에서 state 보관

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,20 +2,20 @@ import React from 'react';
 import './Square.css';
 
 export class Square extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: null,
-    }
-  }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     value: null,
+  //   }
+  // }
   render() {
     return (
       <button className='square' onClick={() => {
-        this.setState({value: 'X'})
+        // this.setState({value: 'X'})
         // console.log('click', this.props.value)
       }}>
-        {this.state.value}
-        {/* {this.props.value} */}
+        {/* {this.state.value} */}
+        {this.props.value}
       </button>
     )
   }
