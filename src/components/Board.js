82120fc59d4613a25caa8cc3f451eddd07df1a26
yogoloper <jguddnjs@gmail.com>
2024-02-14 22:08:07 +0900

    승자 결정 추가

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,11 +5,40 @@ import './Board.css';
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
   const [xIsNext, setXIsNext] = useState(true)
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ]
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] && squares[a] === squares[b] && squares[a] === squares[c]
+      ) {
+        return squares[a]
+      }
+    }
+    return null
+  }
   
-  const status = `Next player: ${xIsNext ? 'X' : 'O'}`
+  const winner = calculateWinner(squares)
+  let status
+  if (winner) {
+    status = `Winner: ${winner}`
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`
+  }
 
   const handleClick = (i) => {
-    const newSquares = squares.slice();
+    const newSquares = squares.slice()
+    if(winner || newSquares[i]) return
     newSquares[i] = xIsNext ? 'X' : 'O'
     setSquares(newSquares)
     // setXIsNext(!xIsNext)
@@ -19,6 +48,8 @@ const Board = () => {
   const renderSquare = (i) => {
     return <Square value={squares[i]} onClick={() => handleClick(i)} />
   }
+
+  
 
   return (
     <div>
