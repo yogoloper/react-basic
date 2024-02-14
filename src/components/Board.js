ae3bbd69a14543a13b62e60f1ea1ec891ecb1549
yogoloper <jguddnjs@gmail.com>
2024-02-14 21:53:19 +0900

    플레이어 순서 변경 추가

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,11 +4,16 @@ import './Board.css';
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
+  const [xIsNext, setXIsNext] = useState(true)
+  
+  const status = `Next player: ${xIsNext ? 'X' : 'O'}`
 
   const handleClick = (i) => {
     const newSquares = squares.slice();
-    newSquares[i] = 'X'
+    newSquares[i] = xIsNext ? 'X' : 'O'
     setSquares(newSquares)
+    // setXIsNext(!xIsNext)
+    setXIsNext(previousState => !previousState)
   }
 
   const renderSquare = (i) => {
@@ -17,7 +22,7 @@ const Board = () => {
 
   return (
     <div>
-      <div className='status'>Next Player: X, O</div>
+      <div className='status'>{status}</div>
       <div className='board-row'>
         {renderSquare(0)}
         {renderSquare(1)}
