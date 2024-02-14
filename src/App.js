7ab5df0774437ff0dad467e9b7b7fab4044c0ebe
yogoloper <jguddnjs@gmail.com>
2024-02-14 14:47:16 +0900

    css 기본 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import './App.css'
 function App() {
   return (
-    <div>
-
+    <div className='game'>
+      <div className='game-board'>
+        game-board
+      </div>
+      <div className='game-info'>
+        game-info
+      </div>
     </div>
   );
 }
