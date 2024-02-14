34a9ddfe3c4a3c7acab47664cb54dd9b0f5f24fd
yogoloper <jguddnjs@gmail.com>
2024-02-14 15:47:36 +0900

    square에 state 추가

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './Square.css';
 
 export class Square extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: null,
+    }
+  }
   render() {
     return (
       <button className='square' onClick={() => {
-        console.log('click', this.props.value)
+        this.setState({value: 'X'})
+        // console.log('click', this.props.value)
       }}>
-        {this.props.value}
+        {this.state.value}
+        {/* {this.props.value} */}
       </button>
     )
   }
