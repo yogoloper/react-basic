38c3dbcc26d99cf80de2828d0364259f7e4d19bb
yogoloper <jguddnjs@gmail.com>
2024-02-14 15:24:44 +0900

    square 컴포넌트에 클릭 이벤트 추가

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,7 +4,9 @@ import './Square.css';
 export class Square extends React.Component {
   render() {
     return (
-      <button className='square'>
+      <button className='square' onClick={() => {
+        console.log('click', this.props.value)
+      }}>
         {this.props.value}
       </button>
     )
